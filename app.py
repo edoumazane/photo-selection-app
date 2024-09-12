90cd5602b676804623445f5a457b6ffe0f83a59f
Etienne Doumazane <etienne.doumazane@gmail.com>
2024-09-12 22:11:38 +0200

    initial commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Directory to store results
-RESULTS_FILE = os.path.join('data', 'results.csv')
+RESULTS_FILE = os.path.join('data', f'results.csv')
 
 # List of 100 images for a 6x5 grid
 images = [f'img{i:02}.png' for i in range(1, 101)]
@@ -15,37 +15,37 @@ images = [f'img{i:02}.png' for i in range(1, 101)]
 os.makedirs('data', exist_ok=True)
 if not os.path.exists(RESULTS_FILE):
     with open(RESULTS_FILE, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Username', 'Timestamp', 'Selected Photos'])
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow(['Timestamp', 'Task', 'Selected Images'])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get the username and selected photos from the form
-        username = request.form.get('username')
+        # Get the task and selected photos from the form
+        task = request.form.get('task')
         selected_photos = request.form.getlist('photo')
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         # Save the results to a CSV file
         with open(RESULTS_FILE, 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([username, timestamp, ", ".join(selected_photos)])
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow([timestamp, task, ",".join(selected_photos)])
 
         # Redirect to the result page with selected photos
-        return redirect(url_for('result', photos=",".join(selected_photos), username=username, timestamp=timestamp))
+        return redirect(url_for('result', photos=",".join(selected_photos), task=task, timestamp=timestamp))
 
     # Render the index.html template
     return render_template('index.html', images=images)
 
 @app.route('/result')
 def result():
-    # Get the selected photos, username, and timestamp from the query parameters
+    # Get the selected photos, task, and timestamp from the query parameters
     selected_photos = request.args.get('photos').split(',')
-    username = request.args.get('username')
+    task = request.args.get('task')
     timestamp = request.args.get('timestamp')
 
     # Render the result.html template
-    return render_template('result.html', photos=selected_photos, username=username, timestamp=timestamp)
+    return render_template('result.html', photos=selected_photos, task=task, timestamp=timestamp)
 
 @app.route('/download')
 def download_csv():
